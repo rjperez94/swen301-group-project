4c4b6559127ddaa82399aabf1d6aa149869ddb93
Ronni Perez <rjperez94@gmail.com>
2016-04-29 20:04:12 +1200

    First-time signup now works (even though password is stored in plain text)

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,33 @@ var router = express.Router();
 
 var users;
 
+//check if file exist
+function getFileRealPath(path){
+    try {
+      return fs.realpathSync(path);
+    } catch(e){
+      return false;
+    }
+}
+
+//check username
+function getIndex(name){
+  if (users) {
+    for (var i = 0; users.groups.length; i++) {
+      if (users.groups[i].user === name) {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
+
+//check pass
+function getPassword(i){
+  return users.groups[i].pass;
+}
+
+
 //router.use(express.cookieParser());
 router.use(session({
   secret: '1234567890QWERTY',
@@ -13,18 +40,26 @@ router.use(session({
   saveUninitialized: true,
 }));
 
-fs.open('users.txt', 'rs+', function(err, fd) {
-   if (err) {
-       return console.error(err);
-   } else {
-     fs.readFile('users.txt', function (err, data) {
-      if (err) {
+if(getFileRealPath('users.json')) {
+  fs.open('users.json', 'rs+', function(err, fd) {
+     if (err) {
          return console.error(err);
-      }
-      users = JSON.parse(data.toString());
-    });
-   }
-});
+     } else {
+       fs.readFile('users.json', function (err, data) {
+        if (err) {
+           return console.error(err);
+        }
+        users = JSON.parse(data.toString());
+        fs.close(fd, function(err){
+          if (err){
+             console.log(err);
+          }
+          console.log("File closed successfully.");
+       });
+      });
+     }
+  });
+};
 
 //var data = fs.readFileSync('users.txt');
 //var users = data.toSring();
@@ -43,14 +78,21 @@ router.get('/', function(req, res) {
 });
 
 router.post('/login', function(req, res) {
-    if (!sess.user) { // Check if user logged in
+    if (!req.session.user) { // Check if user logged in
         var name = req.body.usernameinput;
         var pass = req.body.passwordinput;
-        if (name === 'Manager' && users[0].pass === pass || name === 'Clerk' && users[1].pass === pass) {
-          req.session.user = name;
-          res.render('index/main', {
-            title: 'KPSmart - Home'
-          });
+        var i = getIndex(name);
+        if (i >=0) {
+          if (users[i].groups.pass === pass) {
+            req.session.user = name;
+            res.render('index/main', {
+              title: 'KPSmart - Home'
+            });
+          } else {
+            res.render('index/index', {
+              title: 'KPSmart - Restricted Access'
+            });
+          }
         } else {
           res.render('index/index', {
             title: 'KPSmart - Restricted Access'
@@ -71,12 +113,33 @@ router.post('/signup', function(req, res) {
   //email regex
   var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  if (users[0].user === role || users[1].user === role || pass !== rePass || !re.test(email)) {
+  if (users) {
+    for (var i = 0; i <users.groups.length; i++) {
+      if (users.groups[i].user === role) {
+        res.render('index/index', {
+          title: 'KPSmart - Restricted Access'
+        });
+      }
+    }
+  }
+
+  if (pass !== rePass || !re.test(email)) {
     res.render('index/index', {
       title: 'KPSmart - Restricted Access'
     });
   } else {
-
+    var text = '{"groups":[';
+    if (users) {
+      for (var i = 0; i <users.groups.length; i++) {
+        text+='{"user":"'+users.groups[i].user+'", "pass":"'+users.groups[i].pass+'", "email":"'+users.groups[i].email+'"},';
+      }
+    }
+    text+='{"user":"'+role+'", "pass":"'+pass+'", "email":"'+email+'"}]}';
+    fs.writeFile('users.json', text,  function(err) {
+       if (err) {
+           return console.error(err);
+       }
+    });
   }
 });
 
